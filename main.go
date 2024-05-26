6269ae3ea8edd33e80193fae850068b0b87deb9b
Mani Movassagh <mani.mgh@gmail.com>
2024-05-26 14:50:11 +0200

    easy

---
+++ main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+	"context"
+	"os"
+)
+
+func main() {
+	component := Hello("John")
+	err := component.Render(context.Background(), os.Stdout)
+	if err != nil {
+		return
+	}
+}
